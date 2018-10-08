c2cb7a52b3d30eebc67789807f05bcea22229e8c
SkyeN <skye@dames.es>
2018-10-08 16:11:20 -0700

    import jquery and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { pingPong } from './ping-pong';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
